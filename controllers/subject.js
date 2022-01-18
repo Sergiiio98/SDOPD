de9e8401404189d13949d5580a9e37fc5b7d1f9f
Alan <alan.kazek@gmail.com>
2022-01-18 20:46:55 +0100

    subject api change

--- controllers/subject.js
+++ controllers/subject.js
@@ -1,22 +1,33 @@
 const mongoose = require('mongoose');
 
 const Subject = require('../models/subject');
+const Question = require('../models/question');
+
 
 
 exports.getSubjects = async (req, res) => {
-    const users = await Subject.find({})
-    console.log(users);
-    res.send(users);
+    // const users = await Subject.find({})
+    // console.log(users);
+    // res.send(users);
 
-    // const csvFilePath='./subjects.csv';
-    // const csv=require('csvtojson');
-    // csv()
-    // .fromFile(csvFilePath)
-    // .then((jsonObj)=>{
-    //     console.log(jsonObj);
-    //     res.send(jsonObj);
+    const allQuestion = await Question.find({});
+    console.log(allQuestion);
+    const csvFilePath='./subjects.csv';
+    const csv=require('csvtojson');
+    
+    csv()
+    .fromFile(csvFilePath)
+    .then((jsonObj)=>{
+        // console.log(jsonObj);
+        jsonObj = jsonObj.map(it =>{
+            it.questions = [];
+            it.questions = allQuestion.filter(q => q.subjectId == it.id);
+            return it;
+        })
+        console.log(jsonObj);
+        res.send(jsonObj);
 
-    // })
+    })
     
 
 };
